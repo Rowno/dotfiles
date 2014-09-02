098e31b92f323930c8ccaaf23088d8c610a71597
Roland Warmerdam <rowno@webspirited.com>
2014-09-02 20:25:08 +1200

    Update the global npm packages

--- npm.sh
+++ npm.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
-npm i -g david
-npm i -g grasp
-npm i -g grunt-cli
-npm i -g gulp
-npm i -g jshint
-npm i -g jsxhint
-npm i -g node-inspector
-npm i -g trash
-npm i -g yo
+npm install -g bower
+npm install -g david
+npm install -g grasp
+npm install -g grunt-cli
+npm install -g gulp
+npm install -g jshint
+npm install -g jsxhint
+npm install -g modulus
+npm install -g node-inspector
+npm install -g npm
+npm install -g tessel
+npm install -g trash
+npm install -g vtop
+npm install -g yo
