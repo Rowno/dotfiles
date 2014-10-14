72555e46f00dcaefc1f81afd47dfc4fc370e30ad
Roland Warmerdam <rowno@webspirited.com>
2014-10-14 22:22:55 +1300

    npm.sh: update package list

--- npm.sh
+++ npm.sh
@@ -2,15 +2,12 @@
 
 npm install -g bower
 npm install -g david
-npm install -g grasp
 npm install -g grunt-cli
 npm install -g gulp
 npm install -g jshint
 npm install -g jsxhint
-npm install -g modulus
 npm install -g node-inspector
 npm install -g npm
-npm install -g tessel
 npm install -g trash
 npm install -g vtop
 npm install -g yo
