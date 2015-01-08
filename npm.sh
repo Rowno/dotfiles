ceb24736971ef2f0a830b02d451825f713941f6d
Roland Warmerdam <rowno@webspirited.com>
2015-01-08 15:36:12 +1300

    npm.sh: add castnow and tcpie

--- npm.sh
+++ npm.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 npm install -g bower
+npm install -g castnow
 npm install -g david
 npm install -g grunt-cli
 npm install -g gulp
@@ -8,6 +9,7 @@ npm install -g jshint
 npm install -g jsxhint
 npm install -g node-inspector
 npm install -g npm
+npm install -g tcpie
 npm install -g trash
 npm install -g vtop
 npm install -g yo
