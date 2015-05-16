ca31000fe588a6205103ebcb20815f26efbe4668
Roland Warmerdam <rowno@webspirited.com>
2015-05-16 16:08:55 +1200

    npm.sh: add @rowno/generator-node

--- npm.sh
+++ npm.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
+npm install -g @rowno/generator-node
 npm install -g bower
 npm install -g castnow
 npm install -g david
+npm install -g eslint
 npm install -g grunt-cli
 npm install -g gulp
-npm install -g eslint
 npm install -g jshint
 npm install -g node-inspector
 npm install -g npm
