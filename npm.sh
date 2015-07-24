92fc34649da7df94446cb367a1efa439cb5a718d
Roland Warmerdam <rowno@webspirited.com>
2015-07-25 01:17:32 +1200

    npm: add n and dependency-check

--- npm.sh
+++ npm.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
 
+npm install -g npm
+
 npm install -g @rowno/generator-node
 npm install -g bower
 npm install -g castnow
 npm install -g david
+npm install -g dependency-check
 npm install -g eslint
 npm install -g grunt-cli
 npm install -g gulp
 npm install -g jshint
+npm install -g n
 npm install -g node-inspector
-npm install -g npm
 npm install -g tcpie
 npm install -g trash
 npm install -g vtop
 npm install -g yo
+
+n 0.10
+n 0.12
+n io latest
+npm install -g npm
