a1d66997bf50d817ad1deca7057f5917ac79e7e8
Roland Warmerdam <rowno@webspirited.com>
2015-10-15 15:51:37 +1300

    npm.sh: remove bower and io and update trash

--- npm.sh
+++ npm.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env zsh
 
-npm install -g npm
-
 npm install -g @rowno/generator-node
-npm install -g bower
 npm install -g castnow
 npm install -g david
 npm install -g dependency-check
@@ -15,11 +12,11 @@ npm install -g n
 npm install -g node-inspector
 npm install -g speed-test
 npm install -g tcpie
-npm install -g trash
+npm install -g trash-cli
 npm install -g vtop
 npm install -g yo
 
 n 0.10
-n io latest
 n 0.12
+n latest
 npm install -g npm
