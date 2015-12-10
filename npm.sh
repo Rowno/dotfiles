2f574abb0654adb58d4d79c74b7b93c18c5b7451
Roland Warmerdam <rowno@webspirited.com>
2015-12-10 21:33:49 +1300

    npm.sh: remove castnow and vtop and add requiresafe

--- npm.sh
+++ npm.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env zsh
 
+npm install -g n
+n 0.10
+n 0.12
+n latest
+npm install -g npm
+
 npm install -g @rowno/generator-node
-npm install -g castnow
 npm install -g david
 npm install -g dependency-check
 npm install -g fkill-cli
 npm install -g grunt-cli
 npm install -g gulp
 npm install -g is-online-cli
-npm install -g n
 npm install -g node-inspector
+npm install -g requiresafe
 npm install -g speed-test
 npm install -g tcpie
 npm install -g trash-cli
-npm install -g vtop
 npm install -g yo
-
-n 0.10
-n 0.12
-n latest
-npm install -g npm
