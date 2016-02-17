ce737bb32df3c0fcd476a805379e623b7a9dba7e
Roland Warmerdam <rowno@webspirited.com>
2016-02-17 16:47:58 +1300

    osx.sh: add git-lfs and vagrant

--- osx.sh
+++ osx.sh
@@ -5,8 +5,10 @@
 brew update
 brew install ansible
 brew install git
+brew install git-lfs
 brew install htop-osx
 brew install node
 brew install zsh-completions
+brew cask install vagrant
 
 defaults write -app 'sketch' ApplePersistence -bool no
