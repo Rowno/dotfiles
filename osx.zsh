a71fd50345ef8e19fcc863492fb8c83999a9a3b4
Roland Warmerdam <rowno@webspirited.com>
2016-03-17 23:51:40 +1300

    osx.sh: add gui programs

--- osx.zsh
+++ osx.zsh
@@ -13,4 +13,26 @@ brew cask install vagrant
 brew cask install caskroom/fonts/font-hack
 brew cask install caskroom/fonts/font-roboto
 
+brew cask install 1password
+brew cask install android-file-transfer
+brew cask install atom
+brew cask install caskroom/versions/atom-beta
+brew cask install caskroom/versions/google-chrome-canary
+brew cask install caskroom/versions/iterm2-beta
+brew cask install crashplan
+brew cask install discord
+brew cask install divvy
+brew cask install dropbox
+brew cask install firefox
+brew cask install google-chrome
+brew cask install imagealpha
+brew cask install imageoptim
+brew cask install istat-menus
+brew cask install licecap
+brew cask install logitech-options
+brew cask install sketch
+brew cask install steam
+brew cask install tunnelblick
+brew cask install virtualbox
+
 defaults write -app 'sketch' ApplePersistence -bool no
