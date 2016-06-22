dbae46b01cc5d82632b1ac2c9888a902b8bd2446
Roland Warmerdam <roland@segment.com>
2016-06-22 16:27:30 -0700

    npm.sh: add emoj and now

--- npm.zsh
+++ npm.zsh
@@ -8,11 +8,13 @@ npm install -g @rowno/generator-node
 npm install -g david
 npm install -g dependency-check
 npm install -g diff-so-fancy
+npm install -g emoj
 npm install -g fkill-cli
 npm install -g grunt-cli
 npm install -g gulp-cli
 npm install -g is-online-cli
 npm install -g node-inspector
+npm install -g now
 npm install -g nsp
 npm install -g speed-test
 npm install -g tcpie
