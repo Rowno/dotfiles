ffd45b200a65285a1d849648a3889c05d860bffe
Roland Warmerdam <rowno@webspirited.com>
2016-08-10 21:45:33 -0700

    osx.sh: update brew packages

--- osx.zsh
+++ osx.zsh
@@ -5,8 +5,9 @@
 brew update
 brew install ansible
 brew install git
-brew install git-lfs
+brew install gnupg
 brew install htop
+brew install keybase
 brew install node
 brew install homebrew/dupes/less # Latest less supports emoji!
 brew cask install ngrok
@@ -14,24 +15,29 @@ brew cask install vagrant
 brew cask install caskroom/fonts/font-hack
 brew cask install caskroom/fonts/font-roboto
 
-brew cask install 1password
-brew cask install arq
-brew cask install android-file-transfer
-brew cask install atom
-brew cask install caskroom/versions/atom-beta
-brew cask install caskroom/versions/google-chrome-canary
-brew cask install caskroom/versions/iterm2-beta
-brew cask install divvy
-brew cask install dropbox
-brew cask install firefox
-brew cask install google-chrome
-brew cask install imagealpha
-brew cask install imageoptim
-brew cask install istat-menus
-brew cask install logitech-options
-brew cask install sketch
-brew cask install steam
-brew cask install tunnelblick
-brew cask install virtualbox
+# brew cask install 1password
+# brew cask install android-file-transfer
+# brew cask install arq
+# brew cask install atom
+# brew cask install caskroom/versions/google-chrome-canary
+# brew cask install daisydisk
+# brew cask install dropbox
+# brew cask install firefox
+# brew cask install flux
+# brew cask install gitup
+# brew cask install google-chrome
+# brew cask install google-drive
+# brew cask install imagealpha
+# brew cask install imageoptim
+# brew cask install istat-menus
+# brew cask install iterm2
+# brew cask install logitech-options
+# brew cask install moom
+# brew cask install radio-silence
+# brew cask install sketch
+# brew cask install steam
+# brew cask install tunnelblick
+# brew cask install virtualbox
+# brew cask install vlc
 
 defaults write -app 'sketch' ApplePersistence -bool no
