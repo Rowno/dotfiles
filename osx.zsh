2a160c684faf095d5ba429760b343eeab08208b0
Roland Warmerdam <rowno@webspirited.com>
2017-12-07 22:16:42 -0800

    Update osx.sh

--- osx.zsh
+++ osx.zsh
@@ -7,30 +7,30 @@ brew install ansible
 brew install git
 brew install gnupg
 brew install htop
+brew install micro
 brew install node
 brew install yarn
-brew install homebrew/dupes/less # Latest less supports emoji!
 brew cask install keybase
 brew cask install ngrok
 brew cask install caskroom/fonts/font-hack
 brew cask install caskroom/fonts/font-roboto
-brew cask install insomnia
 
 # brew cask install 1password
 # brew cask install android-file-transfer
 # brew cask install arq
 # brew cask install atom
-# brew cask install caskroom/versions/google-chrome-canary
 # brew cask install daisydisk
 # brew cask install dropbox
 # brew cask install firefox
 # brew cask install flux
+# brew cask install google-backup-and-sync
 # brew cask install google-chrome
-# brew cask install google-drive
+# brew cask install google-chrome-canary
 # brew cask install gpgtools
 # brew cask install hyper
 # brew cask install imagealpha
 # brew cask install imageoptim
+# brew cask install insomnia
 # brew cask install istat-menus
 # brew cask install kap
 # brew cask install logitech-options
