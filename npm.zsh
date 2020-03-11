a2d05d1d3b8cbdacbfb7451253756c7bb851da51
Roland Warmerdam <rowno@webspirited.com>
2020-03-10 22:17:03 -0700

    Use a single npm install command

--- npm.zsh
+++ npm.zsh
@@ -3,22 +3,23 @@
 npm config set sign-git-tag true
 yarn config set version-sign-git-tag true
 
-npm install -g npm
-npm install -g @rowno/generator-node
-npm install -g depcheck
-npm install -g diff-so-fancy
-npm install -g emoj
-npm install -g fkill-cli
-npm install -g is-online-cli
-npm install -g now
-npm install -g np
-npm install -g react-benchmark
-npm install -g serve
-npm install -g source-map-explorer
-npm install -g speed-test
-npm install -g tcpie
-npm install -g to-ico-cli
-npm install -g trash-cli
-npm install -g webpack-bundle-analyzer
-npm install -g webpack-bundle-size-analyzer
-npm install -g yo
+npm install -g \
+  npm \
+  @rowno/generator-node \
+  depcheck \
+  diff-so-fancy \
+  emoj \
+  fkill-cli \
+  is-online-cli \
+  now \
+  np \
+  react-benchmark \
+  serve \
+  source-map-explorer \
+  speed-test \
+  tcpie \
+  to-ico-cli \
+  trash-cli \
+  webpack-bundle-analyzer \
+  webpack-bundle-size-analyzer \
+  yo
