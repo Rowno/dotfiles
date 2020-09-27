57a5dc9ace5ccbd1b4896df6f4a2824ccac9f082
Roland Warmerdam <rowno@webspirited.com>
2020-09-26 18:15:27 -0700

    Automatically install global packages when installing a new Node version

--- npm.zsh
+++ npm.zsh
@@ -3,22 +3,4 @@
 npm config set sign-git-tag true
 yarn config set version-sign-git-tag true
 
-npm install -g \
-  npm \
-  @rowno/generator-node \
-  depcheck \
-  diff-so-fancy \
-  emoj \
-  fkill-cli \
-  is-online-cli \
-  np \
-  react-benchmark \
-  serve \
-  source-map-explorer \
-  speed-test \
-  tcpie \
-  to-ico-cli \
-  trash-cli \
-  webpack-bundle-analyzer \
-  webpack-bundle-size-analyzer \
-  yo
+cat ./public/.dotfiles/nvm/default-packages | xargs npm install -g
